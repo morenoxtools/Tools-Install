99609c729ab1dadeb693caa1180c6074448265a5
M0R3N0xW <50466134+M0R3N0xW@users.noreply.github.com>
2019-05-09 22:11:39 +0000

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,322 @@
+#TOOLS INSTALL V1
+#CODED BY : M0R3N0
+#JANGAN DI UBAH AUTHORNYA
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo "-----------------------------------------------------------"
+toilet -f pagga "   TOOLS INSTALL"|lolcat
+echo "-----------------------------------------------------------"
+echo $ij"[+]─────────────────────────────────────────────────────[+]"
+echo $ij" | ••••••••••       |Tools_Install|      ••••••••••••  |"
+echo $ij" | ───────────────────────────────────────────────────── |"
+echo $ij" | VERSION TOOL: INSTALL V1                              |"
+echo $ij" | Author      : M0R3N0                                  |"                     >
+echo $ij" | Instagram   : mxw_white_hat                           |"
+echo $ij" | Github      : https://github.com/M0R3N0               |"
+echo $ij" | Thanks To   : All Member Cyber Indo                   |"
+echo $ij" | COPYRIGHT   : 2019 Cyber Indo                         |"
+echo $ij"[+]─────────────────────────────────────────────────────[+]"
+echo
+echo $pu"───────────────────────────────────────────"
+echo $or"[00]" $pu"About" $ku"Tool" $ij"Program"
+echo $pu"───────────────────────────────────────────"
+echo $pu"───────────────────────────────────────────"
+echo $me"        Cyber Indo×Tool"
+echo $pu"───────────────────────────────────────────"
+echo $cy"[01]" $ku"SPAM-CALL                            |"
+echo $cy"[02]" $pu"Yt-Downloader                        |"
+echo $cy"[03]" $me"DORK-SCANNER                         |"
+echo $cy"[04]" $pr"REV-IP                               |"
+echo $cy"[05]" $ij"CHECK-IP                             |"
+echo $cy"[06]" $bi"INSTAHACK                            |"
+echo $cy"[07]" $or"AdminFinder                          |"
+echo $cy"[08]" $ku"DDoS                                 |"
+echo $cy"[09]" $pu"MD5-CRACKER                          |"
+echo $cy"[10]" $me"CAPING-BOT                           |"
+echo $cy"[11]" $pr"MAIL-SPAMMER                         |"
+echo $cy"[12]" $ij"Im3-Spammer                          |"
+echo $cy"[13]" $bi"Create-Bot-SSH                       |"
+echo $cy"[14]" $or"ghoul                                |"
+echo $cy"[15]" $ku"SQLI-Vuln-Checker                    |"
+echo $cy"[16]" $pu"Wp-Scan                              |"
+echo $cy"[17]" $me"NAS                                  |"
+echo $cy"[18]" $pr"Mp4-Convert                          |"
+echo $cy"[19]" $ij"Exploit-LokoMedia                    |"
+echo $cy"[20]" $bi"DDoS-With-Perl                       |"
+echo $pu"───────────────────────────────────────────"
+echo $me"         INSTALL BAHANNYA DULU "
+echo $pu"───────────────────────────────────────────"
+echo $cy"[99]" $or"PILIH AKU SAYANG"
+echo $pu"───────────────────────────────────────────"
+echo $me"┌==="$bi"["$i"IY×RenCode"$bi"]"$me"======"$bi"["$i""SELECT THE NUMBER""$bi"]"
+echo $me"¦"
+read -p"└──# " kaex
+
+if [ $kaex = 1 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/SPAM-CALL
+cd SPAM-CALL
+bash CaLL.sh
+fi
+
+if [ $kaex = 2 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/YOUTUBE-DOWNLOADER
+cd YOUTUBE-DOWNLOADER
+python2 youtube.py
+fi
+
+if [ $kaex = 3 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/DORK-SCANNER
+cd DORK-SCANNER
+php scan.php
+fi
+
+if [ $kaex = 4 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/REV-IP
+cd REV-IP
+python3 rev.io
+fi
+
+if [ $kaex = 5 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/CHECK-IP
+cd CHECK-IP
+python2 checkip.py
+fi
+
+if [ $kaex = 6 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/INSTAHACK
+cd INSTAHACK
+python2 insta.py
+fi
+
+if [ $kaex = 7 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/AdminFinder
+cd AdminFinder
+python2 admin.py
+fi
+
+if [ $kaex = 8 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/DDoS
+cd DDoS
+python2 ddos.py
+fi
+
+if [ $kaex = 9 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/MD5-CRACKER
+cd MD5-CRACKER
+python2 md5.py
+fi
+
+if [ $kaex = 10 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/CAPING-BOT
+cd CAPING-BOT
+php bot.php
+fi
+
+if [ $kaex = 11 ]
+then
+cleaar
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/MAIL-SPAMMER
+cd MAIL-SPAMMER
+php mail.php
+fi
+
+if [ $kaex = 12 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/Im3-Spammer
+cd Im3-Spammer
+php im3.php
+fi
+
+if [ $kaex = 13 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/CREATE-BOT-SSH
+cd CREATE-BOT-SSH
+python2 ssh.py
+fi
+
+if [ $kaex = 14 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/ghoul
+cd ghoul
+python3 ghoul.py
+fi
+
+if [ $kaex = 15 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/SQLI-Vuln-Checker
+cd SQLI-Vuln-Checker
+python3 sqli.py
+fi
+
+if [ $kaex = 16 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/Wp-Scan
+cd Wp-Scan
+python2 auto.py
+fi
+
+if [ $kaex = 14 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/ghoul
+cd ghoul
+python3 ghoul.py
+fi
+
+if [ $kaex = 15 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/SQLI-Vuln-Checker
+cd SQLI-Vuln-Checker
+python3 sqli.py
+fi
+
+if [ $kaex = 16 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/Wp-Scan
+cd Wp-Scan
+python2 auto.py
+fi
+
+if [ $kaex = 17 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/NAS
+cd NAS
+python3 sabyan.chan
+fi
+
+if [ $kaex = 18 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/Mp4-Convert
+cd Mp4-Convert
+python2 tube.py
+fi
+
+if [ $kaex = 19 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/Exploit-Lokomedia
+cd Exploit-Lokomedia
+python2 Loko.py
+fi
+
+if [ $kaex = 19 ]
+then
+clear
+figlet -f slant "[PLEASE WAIT]"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/Exploit-Lokomedia
+cd Exploit-Lokomedia
+python2 Loko.py
+fi
+
+if [ $kaex = 99 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install python2 && pkg install python
+pip2 install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install dirhunt
+pip2 install youtube-dl
+pkg install curl
+pkg install php
+pip2 install termcolor
+pip2 install bs4
+pip2 install beautifulsoup
+pip2 install colorama
+pkg install perl
+pkg install ruby
+pip install requests
+pkg install figlet
+fi
+
+if [ $kaex = 00 ]
+then
+clear
+echo $pu"───────────────────────────────────────────"
+echo $cy"NAME      : M0R3N0"
+echo $or"CODENAME  : MUH4K3M0S"
+echo $ku"REGION    : KALIMANTAN BARAT"
+echo $pu"───────────────────────────────────────────"
+fi
